cdfe2f714e28062635d285d835f818cc5dac0a09
pangs <604648413@qq.com>
2020-02-20 22:15:37 +0800

    HolleWorld

---
+++ HolleWorld.java
@@ -0,0 +1,9 @@
+public class HolleWorld{
+	public static void main(String[] args) throws Exception {
+		System.out.println("HolleWorld");
+		System.out.println("1");
+		System.out.println("HolleWorld");
+		System.out.println("2");
+	}
+}
+	
